2cdff0e903416dc81a56d12afe613da067b0266b
zoudongxu <zoudongxu@ichunqiu.com>
2018-09-02 09:58:09 +0800

    fix bug helpers

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,12 +1,12 @@
 <?php
 function get_db_config() {
-	if (getenv(IS_IN_HEROKU)) {
+	if (getenv('IS_IN_HEROKU')) {
 		$url = parse_url(getenv("DATABASE_URL"));
 
 		return $db_config = [
 			'connection' => 'pgsql',
 			'host'       => $url['host'],
-			'databse'    => substr($url['path'], 1),
+			'database'   => substr($url['path'], 1),
 			'username'   => $url['user'],
 			'password'   => $url['pass'],
 		];
@@ -14,7 +14,7 @@ function get_db_config() {
 		return $db_config = [
 			'connection' => env('DB_CONNECTION', 'mysql'),
 			'host'       => env('DB_HOST', 'localhost'),
-			'databse'    => env('DB_DATABASE', 'sample'),
+			'database'   => env('DB_DATABASE', 'sample'),
 			'username'   => env('DB_USERNAME', 'homestead'),
 			'password'   => env('DB_PASSWORD', 'secret'),
 		];
