2dc772662cdd7814c8852a78fc4051e9f278e5bc
zoudongxu <dorsey.joe@icloud.com>
2018-09-03 13:16:56 +0000

    Finish user edit, update, index, and destroy actions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,33 +4,83 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Auth;
 
 class UserController extends Controller {
-	public function create(Request $request) {
-		return view('users.create');
-	}
+    public function __construct()
+    {
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store'],
+        ]);
 
-	public function show(User $user) {
-		return view('users.show', compact('user'));
-	}
+        $this->middleware('guest', [
+            'only' => ['create'],
+        ]);
+    }
 
-	public function store(Request $request) {
-		$this->validate($request, [
-				'name'     => 'required|max:50',
-				'email'    => 'required|email|unique:users|max:255',
-				'password' => 'required|confirmed|min:6',
-			]);
+    public function index()
+    {
+        $users = User::paginate(10);
+        return view('users.index', compact('users'));
+    }
 
-		$user = User::create([
-				'name'     => $request->name,
-				'email'    => $request->email,
-				'password' => bcrypt($request->password),
-			]);
+    public function create(Request $request) {
+        return view('users.create');
+    }
 
-		Auth::login($user);
+    public function edit(User $user) {
+        $this->authorize('update', $user);
+        return view('users.edit', compact('user'));
+    }
 
-		session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
-		return redirect()->route('users.show', [$user]);
-	}
+    public function show(User $user) {
+        return view('users.show', compact('user'));
+    }
 
+    public function store(Request $request) {
+        $this->validate($request, [
+                'name'     => 'required|max:50',
+                'email'    => 'required|email|unique:users|max:255',
+                'password' => 'required|confirmed|min:6',
+            ]);
+
+        $user = User::create([
+                'name'     => $request->name,
+                'email'    => $request->email,
+                'password' => bcrypt($request->password),
+            ]);
+
+        Auth::login($user);
+
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show', [$user]);
+    }
+
+    public function update(Request $request, User $user)
+    {
+        $this->validate($request, [
+            'name'     => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6',
+        ]);
+
+        $this->authorize('update', $user);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功！');
+        return redirect()->route('users.show', $user->id);
+    }
+
+    public function destroy(User $user)
+    {
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+        return back();
+    }
 }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -13,12 +13,16 @@ use Faker\Generator as Faker;
 |
  */
 
-$factory->define(App\Models\User::
-class , function (Faker $faker) {
-		return [
-			'name'           => $faker->name,
-			'email'          => $faker->unique()->safeEmail,
-			'password'       => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-			'remember_token' => str_random(10),
-		];
-	});
+$factory->define(App\Models\User::class , function (Faker $faker) {
+    $datetime = $faker->date . ' ' . $faker->time;
+    static $password;
+	return [
+		'name'            => $faker->name,
+		'email'           => $faker->unique()->safeEmail,
+        'is_admin'        => false,
+		'password'        => $password ?: bcrypt('secret'), // secret
+		'remember_token'  => str_random(10),
+        'created_at'      => $datetime,
+        'updated_at'      => $datetime,
+	];
+});
