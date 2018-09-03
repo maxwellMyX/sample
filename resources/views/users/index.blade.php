45f608b9cc02187ee50d35b94a3506ee4f24a650
zoudongxu <zoudongxu@ichunqiu.com>
2018-09-04 07:16:59 +0800

    css class

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 <div class="col-md-offset-2 col-md-8">
     <h1>所有用户</h1>
-    <ul>
+    <ul class="users">
         @foreach($users as $user)
             @include('users._user')
         @endforeach
