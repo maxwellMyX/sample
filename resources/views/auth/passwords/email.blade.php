8d0645f001ca9c3145efb66a52517f6625fe81d0
zoudongxu <dorsey.joe@icloud.com>
2018-09-05 07:34:44 +0000

    Add password resets & email configuration

---
+++ resources/views/auth/passwords/email.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.default')
+@section('title', '密码重置')
+
+@section('content')
+<div class="col-md-8 col-md-offset-2">
+    <div class="panel panel-default">
+        <div class="panel-heading">密码重置</div>
+        <div class="panel-body">
+            @if (session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
+                </div>
+            @endif
+
+            <form class="form-horizontal" method="POST" action="{{ route('password.email') }}">
+                {{ csrf_field() }}
+
+                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                    <label for="email" class="col-md-4 control-label">邮箱地址:</label>
+                    <div class="col-md-6">
+                        <input type="email" name="email" id="email" class="form-control" value="{{ old('email') }}" required>
+
+                        @if ($errors->has('email'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-md-6 col-md-offset-4">
+                        <button type="submit" class="btn btn-primary">发送密码重置邮件</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+@stop
